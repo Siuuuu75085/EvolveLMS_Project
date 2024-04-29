5108b22ec6147693ffbc49e837176f7811720f0a
ntdung1702 <118947864+tandung1702@users.noreply.github.com>
2024-04-29 15:27:19 +0700

    Create footer.php

---
+++ student/stuInclude/footer.php
@@ -0,0 +1,17 @@
+
+<!-- Jquery and Bosstrap JavaScript -->
+<script typr="text/javascript" src="../js/jquery.min.js"></script>
+<script typr="text/javascript" src="../js/popper.min.js"></script>
+<script typr="text/javascript" src="../js/bootstrap.min.js"></script>
+
+<!-- Font Awesome JS -->
+<script typr="text/javascript" src="../js/all.min.js"></script>
+
+<!--Ajax Call JavaScript -->
+<!-- <script typr="text/javascript" src="../js/ajaxrequest.js"></script> -->
+
+<!-- Custom JavaScript -->
+<script typr="text/javascript" src="../js/script.js"></script>
+</body>
+
+</html>
