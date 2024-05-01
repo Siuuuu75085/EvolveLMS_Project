dd17d0ffa781515fcfd9afe16e4e25f8966f1c4f
Siuuuu75085 <147909040+Siuuuu75085@users.noreply.github.com>
2024-05-01 22:48:15 +0700

    ELearning.php

--- ELearning.php
+++ ELearning.php
@@ -196,7 +196,93 @@
   </div>
   </div>
 </div>
-  <!--End SSection----------------------------------------------------------->
+
+<?php
+$sql = "SELECT course_id FROM course";
+$result = $conn->query($sql);
+while($row = $result->fetch_assoc()){
+if(isset($_REQUEST['checkid']) && $_REQUEST['checkid'] == $row
+['course_id']){
+$sql = "SELECT * FROM course WHERE course_id = {$_REQUEST
+['checkid']}";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+if(($row['course_id']) == $_REQUEST['checkid']){
+$_SESSION['course_id'] = $row['course_id'];
+$_SESSION['course_name'] = $row['course_name'];
+?>
+<h3> class="mt-5 bg-dark text-while p2">Course ID: <?php if(isset($row['course_id'])) { echo $row
+['course_id']; } ?> Course Name: <?php if(isset($row
+['course_name'])) { echo $row['course_name']; } ?> </h3>
+<?php
+
+$sql = "SELECT * FROM lesson WHERE course_id = {$_REQUEST
+    ['checkid']}";
+    $result = $conn->query($sql);
+    echo '<table class="table">
+    <thead>
+    <tr>
+    <th scope="col">Lesson ID</th>
+    <th scope="col">Lesson Name</th>
+    <th scope="col">Lesson Link</th>
+    <th scope="col">Action</th>
+    </tr>
+    </thead>
+    <tbody>';
+    while($row = $result->fetch assoc()){
+echo '<tr>';
+echo '<th scope="row">'.$row["lesson_id"].'</th>';
+echo '<td>'. $row["lesson_name"].'</td>';
+echo '<td>'.$row["lesson_link"].
+echo '<td>
+<form action="editlesson.php" method="POST" class="d-inline">
+<input type="hidden" name="id" value='. $row
+["lesson_id"] .'>
+<button type="submit" class="btn btn-info mr-3"
+name="view" value="View"><i class="fas fa-pen"></i></
+button>
+</form>
+<form action="" method="POST" class="d-inline">
+<input type="hidden" name="id" value='. $row
+["lesson_id"] .'>
+<button type="submit" class="btn btn-secondary" name="delete" value="Delete"><i class="far fa-trash-alt"></i></button>
+</form>
+</td>
+</tr>';
+}
+echo '</tbody>
+</table>';
+} else {
+echo '<div class="alert alert-dark mt-4" role="alert">
+Course Not Found ! </div>';
+}
+
+if(isset($_REQUEST['delete'])){
+    $sql = "DELETE FROM lesson WHERE lesson_id = {$_REQUEST['id']}";
+    if($conn->query($sql) === TRUE) {
+    // echo "Record Deleted Successfully";
+    // below code will refresh the page after deleting the record
+    echo '<meta http-equiv="refresh" content= "0; URL=?deleted" / >';
+    } else {
+    echo "Unable to Delete Data";
+    }
+
+}
+}
+}
+
+?>
+</div>
+
+<?php
+if(isset($_SESSION['course_id'])){
+    echo '<div>
+    <a class="btn btn-danger box" href="./addLesson.php"><i 
+    class="fas fa-plus fa-2x"></i></a>
+    </div>';
+
+}
+
 <!--Footer -->
 <?php
   include('mainPart/footer.php');
